57635297f7f5904340343f9d7bf4b5d8c8c47fee
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-15 20:42:11 +0200

    [LUZ-12] Make Blog with Infinite Scroll the main for Blog Page

---
+++ pages/blog3.js
@@ -0,0 +1,68 @@
+import { gql } from "@apollo/client";
+import React, { Fragment } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import {client} from "../services/apollo";
+
+import { getAllPostsQuery } from "../lib/api/getAllPostsQuery";
+import BlogContainer from "../components/BlogContainer";
+import styles from './blog/blog.module.scss'
+import { NextSeo } from "next-seo";
+const test = ({ data: { data } }) => {
+  const { posts } = data;
+
+  return (
+    <>
+    <NextSeo
+      title={`المدونة - موقع لوز`}
+      description={`مدونة المواضيع المطروحة من موقع لوز!`}
+    />
+    <BlogContainer>
+      {posts.edges.map((post) => {
+        const { featuredImage } = post.node;
+
+        return (
+          <div className={styles.blogContent} key={post.node.id}>
+            <Link href={`/blog/${post.node.slug}`}>
+              <a>
+                {featuredImage && (
+                  <Image
+                    width="350"
+                    height="250"
+                    layout="responsive"
+                    src={featuredImage?.node?.sourceUrl}
+                    blurDataURL={`/_next/image?url=${featuredImage?.node?.sourceUrl}&w=16&q=1`}
+                    placeholder="blur"
+                    loading="lazy"
+                  />
+                )}
+              </a>
+            </Link>
+            <Link href={`/blog/${post.node.slug}`}>
+              <a>
+                <h1>{post.node.title}</h1>
+              </a>
+            </Link>
+            <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+          </div>
+        );
+      })}
+    </BlogContainer>
+    </>
+  );
+};
+
+export default test;
+
+export async function getStaticProps() {
+  const data = await client.query({
+    query: getAllPostsQuery,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
