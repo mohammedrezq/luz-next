5f7c464e50ce996957e71b5b0bdcacab9c563363
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-15 20:02:37 +0200

    [LUZ-12] Added Blog Posts Infinite Scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,30 @@
+import { AppContext, AppInitialProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import Layout from "../components/Layout";
-import {client} from "../services/apollo";
 
+import { useApollo } from "../services/apollo";
+import Layout from "../components/Layout";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <Layout>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
   );
 }
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ApolloProvider client={client}>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </ApolloProvider>
+//   );
+// }
 
 export default MyApp;
