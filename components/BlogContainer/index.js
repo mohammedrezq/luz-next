045680b87d1822a0be9d6d322e9e7cb0a9618764
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-16 18:35:47 +0200

    [LUZ-13] Rename BlogContainer Components Adding Posts per Category

--- components/BlogContainer/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./BlogContainer.module.scss";
-
-const BlogContainer = ({ children }) => {
-  return <div className={styles.BlogContainer}>{children}</div>;
-};
-
-export default BlogContainer;
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,10 +1,57 @@
 import { gql } from "@apollo/client";
 import { getCategoryBySlug } from "../../lib/api/getCategoryBySlug";
 import { initializeApollo } from "../../services/apollo";
+import Image from "next/image";
+import Link from "next/link";
+import { NextSeo } from "next-seo";
+import PostsContainer from "../../components/PostsContainer";
 
-const Category = ({category} = props) => {
-  console.log(category);
-  return <div>Hello Category</div>;
+import styles from '../blog/blog.module.scss'
+
+const Category = ({ category } = props) => {
+  const { posts } = category;
+//   console.log(posts);
+//   console.log(category);
+  return (
+    <>
+      <NextSeo
+        title={`${category.name}`}
+        description={`${category.description ? category.description : category.name}`}
+      />
+
+        <PostsContainer>
+          {posts.edges.map((post) => {
+            const { featuredImage } = post.node;
+
+            return (
+              <div className={styles.blogContent} key={post.node.id}>
+                <Link href={`/blog/${post.node.slug}`}>
+                  <a>
+                    {featuredImage && (
+                      <Image
+                        width="350"
+                        height="250"
+                        layout="responsive"
+                        src={featuredImage?.node?.sourceUrl}
+                        blurDataURL={`/_next/image?url=${featuredImage?.node?.sourceUrl}&w=16&q=1`}
+                        placeholder="blur"
+                        loading="lazy"
+                      />
+                    )}
+                  </a>
+                </Link>
+                <Link href={`/blog/${post.node.slug}`}>
+                  <a>
+                    <h1>{post.node.title}</h1>
+                  </a>
+                </Link>
+                <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+              </div>
+            );
+          })}
+        </PostsContainer>
+    </>
+  );
 };
 
 export default Category;
