d550a2286822f366b89118f382de7fadcc78a9e6
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-20 22:59:31 +0200

    [LUZ-15] Add Menus to Desktop Display

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState, useEffect, useRef, forwardRef } from "react";
 import Link from "next/link";
+import { FaSearch, FaChevronDown } from "react-icons/fa";
 
 import { LIST_MENU_ITEM_PRIMARY } from "../../lib/api/getMenus";
 import { initializeApollo } from "../../services/apollo";
@@ -7,31 +8,83 @@ import { initializeApollo } from "../../services/apollo";
 import styles from "./Header.module.scss";
 
 const Header = (props) => {
+  const [subMenu, setSubMenu] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(null);
+  const subMenuRef = useRef(null);
+
+  const openSubMenuHandler = (index) => {
+    setActiveIndex(index);
+    setSubMenu((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if (
+        subMenuRef.current !== null &&
+        !subMenuRef.current.contains(e.target)
+      ) {
+        setSubMenu(!subMenu);
+      }
+    };
+
+    // If the item is active (ie open) then listen for clicks
+    if (subMenu) {
+      window.addEventListener("click", pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [subMenu]);
+
   return (
     <div className={styles.header}>
       <h1>Header</h1>
-      {props.menus.map((menu, i) => {
-        const siteSubFolder = "newsite";
-        const newPath = menu?.path
-          .toLowerCase()
-          .includes(siteSubFolder.toLowerCase())
-          ? menu?.path.replace("/newsite/", "")
-          : null;
-        return (
-          <div key={menu.id}>
-            <Link href={`/blog/${newPath}`}>{menu.label}</Link>
-            {menu.children.length > 0 &&
-              menu.children.map((submenu, index) => {
-                // console.log(submenu);
-                return (
-                  <div style={{background: "#ccc"}} key={submenu.id}>
-                    <Link href={`/blog/${newPath}`}>{submenu.label}</Link>
-                  </div>
-                );
-              })}
-          </div>
-        );
-      })}
+      <ul className={styles.navMenu}>
+        {props.menus.map((menu, index) => {
+          const siteSubFolder = "newsite";
+          const newPath = menu?.path
+            .toLowerCase()
+            .includes(siteSubFolder.toLowerCase())
+            ? menu?.path.replace("/newsite/", "")
+            : null;
+          return (
+            <li key={menu.id}>
+              <div className={styles.menuItemHasChildren}>
+                {menu.children.length > 0 && (
+                  <>
+                      <button
+                        key={index}
+                        onClick={() => openSubMenuHandler(index)}
+                        key={index}
+                        className={styles.menuItemHasChildrenClosed}
+                      >
+                        <FaChevronDown />
+                      </button>
+                  </>
+                )}
+                    <Link href={`/blog/${newPath}`}>
+                      <a title={menu.title}>{menu.label}</a>
+                    </Link>
+              </div>
+
+              {menu.children.length > 0 && (
+                <ul  ref={subMenuRef} key={index} className={(subMenu && activeIndex === index) ? styles.navSubMenu : styles.navSubMenuClosed}>
+                  {menu.children.map(({id, path, label, title}) => {
+                  return (
+                    <li key={id}>
+                        <Link href={`/blog/${newPath}`}>
+                        <a title={menu.title}>{menu.label}</a>
+                        </Link>
+                    </li>
+                  );
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
